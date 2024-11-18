0244b7f68c90ad9a1693e7dd999c3823d4c9451e
rouzashin245 <rouzashin245@gmail.com>
2024-11-18 04:57:54 +0000

    prakLagi

---
+++ app/Http/Controllers/inventoryController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\logistik;
+use Illuminate\Support\Facades\Storage;
+
+class inventoryController extends Controller
+{
+    public function index(){ //read
+
+        $logistik = logistik::all();
+
+        return view('logistik.logistik', compact('logistik'));
+    }
+    
+    public function create(){ //create
+        return view('logistik.addLogistik');
+    }
+
+    public function store(Request $request){ //store
+        $this -> validate($request, [
+            'foto'=>'required|image|mimes:jpeg,png,jpg',
+            'nama'=>'required',
+            'deskripsi'=>'required',
+        ]);
+
+        $foto = $request->file('foto');
+        $foto -> storeAs('public/foto', $foto->hashName());
+
+        logistik::create([
+            'foto'=> $foto->hashName(),
+            'nama'=> $request->nama,
+            'deskripsi' => $request->deskripsi
+        ]);
+        
+        return redirect()->route('logistik.index');
+    }
+
+    public function edit(logistik $logistik){ //edit
+        return view('logistik.editLogistik', compact('logistik'));
+    }
+
+    public function update(Request $request, logistik $logistik){
+
+        $this -> validate($request, [
+            'foto'=>'required|image|mimes:jpeg,png,jpg',
+            'nama'=>'required',
+            'deskripsi'=>'required',
+        ]);
+
+        if($request -> file ('foto')){
+
+            //upload image
+            $foto = $request->file('foto');
+            $foto -> storeAs('public/foto', $foto->hashName());
+
+            //delete yg lama
+            Storage::delete('public/foto/', $logistik->foto);
+
+            //update inventory dengan new image
+            $logistik->update([
+                'foto'=> $foto->hashName(),
+                'nama'=> $request->nama,
+                'deskripsi'=> $request->deskripsi,
+            ]);
+        } else {
+            //update inven tnpa image
+            $logistik->update([
+                'foto'=> $request->nama,
+                'deskripsi'=> $request->deskripsi,
+            ]);
+        }
+
+        //return
+        return redirect()->route('logistik.index');
+    }
+
+    public function destroy(logistik $logistik){
+        Storage::delete('public/foto/' . $logistik->foto);
+
+        $logistik->delete();
+
+        return redirect()->route('logistik.index');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+@section('beranda')
+        active
+    @endsection
+    @section('content')
+        <div class="container">
+            <div class="row pb-5">
+                <div class="col-6">
+                    <h1 class="text-dark">Selamat Datang di Modul WAD2022</h1>
+                </div>
+            </div>
+        </div>
+@endsection
